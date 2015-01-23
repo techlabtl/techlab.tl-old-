22fb7a8d305cf0919ab54a8f2ff65c9aba5a39b2
Matteo Giaccone <matteo.giaccone@welaika.com>
2015-01-23 19:02:59 +0000

    Clean PR

--- wp-content/themes/techlab/tmp/2071f81705a8fabaecc1add62fe93474.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once '/home/endorama/code/github/techlabtl/techlab.tl/wp-content/plugins/wordless/vendor/phamlp/haml/HamlHelpers.php';
-?><div class="row">
-<div class="col-xs-12">
-<img alt="Techlab logo" class="tl-page-logo" src="/wp-content/themes/techlab/assets/images/logo.svg" /></div></div><div class="row">
-<div class="col-xs-12 tl-page-brand">
-  Associazione Techlab
-</div></div>
